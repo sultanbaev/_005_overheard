b1d733a2c2bec4ddb373ae5ed165c82f2abe9f56
svvitaly <sultanbaev.vitaly@gmail.com>
2020-06-12 01:20:37 +0500

    Initial commit

---
+++ overheard.c
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+ int main(int argc, char **argv) {
+     if(argc <= 1) {
+         printf("\n\n\nSyntax : \n\n\toverheard --vki\n\toverheard --about\n\n\n");
+         return 0;
+     }
+     if(!strcmp(argv[1], "--vki"))
+            {
+                
+printf("\n\n\n                .:+oooo+/-\n");
+printf("              `+o++++ooooo/\n");
+printf("    .:////:.  +oo++++o++++/\n");
+printf("  -+ooo++//+/:o+ooo++o+++oo+/+/....`\n");
+printf(" /o+++++:--:+o++ooooooo+++o+oooo++oo+:`\n");
+printf(":oo++++:-----+oooooooo++++++ooo++++++oo-\n");
+printf("ooooo++------://:----/+o+o+/--:/+o++++++\n");
+printf("s+++oo+-------+/+:-::--:+:::     -o+++o:\n");
+printf("+o+++o+-------:+y-`-../------     o++o/`\n");
+printf("`+ooo++/::-----oM:-+:``/---///    ooo/+-\n");
+printf(" `ooo+oo+oo/--:+mNNd:` /--/::o-   /+o+/\n");
+printf(" /o+++++++oo/---:sdo. `:--yso-.    ```\n");
+printf(" :o+++++++ooo:------...---om/`\n");
+printf("  :oooooooooo/-------------:--\n");
+printf(" `+ooooooo++o/-----------/---.     `.-:////::-`\n");
+printf(" /o++ooooooo+-----------:+```   `:++oooooooooo++-`\n");
+printf(" ++++oooooo:-------...--..    `:ooooooooooooooo+o+-\n");
+printf(" .+o++++o+oo-------     ``````+ooo+ooooo+++++ooo++o-\n");
+printf("  `:+oooo++o------:```--------++osyhdhyoo++++++o++o/\n");
+printf("    /oooo++:----------------.`-:yNMMdso++++++++oo+o-\n");
+printf("    /ooos+:-------------------.-:mMho++++++++++oooo////:-`\n");
+printf("     .--::-----------------------sNs+++++++++ooooo+++++oo+-\n");
+printf("        `:-----------------------+Myo+++++++ooooooooo+++++o:\n");
+printf("         .---------------------.-yMNho+++oooooo++++ooo+++++o\n");
+printf("         .::---------------:-----mMMMmyoooooooooo++++o++++oo\n");
+printf("        -:::::/----:-----:------:hdMMmyoooooooooo++++o+++oo-\n");
+printf("        :::::os-----/::::::`-------hdo++++oooooo+++ooo+oo+.\n");
+printf("        -::::hs-----:::::::- -------o++++++ooooooooooo+:.\n");
+printf("        `::::::------:::::::  :-----:++++++ooooooo+++o+-\n");
+printf("         -:::::------:::::::. -------/o+++++oo++o++++++o:\n");
+printf("         `:::::------:::::::. .------:+o+++++o+ooooo+++o+\n");
+printf("          `::::------:::::::. `-------.-+ooooo+ooooooooo.\n");
+printf("            .::------:::::::` .--------  .-/+oooooooo/.\n");
+printf("             .-------:::::::  ---------      :oooo++o-\n");
+printf("             :-------:`````  .---------      oo++o+/-.::\n");
+printf("            ---------:       `.......`        :+oooooo:\n");
+printf("            ---------`                           `...\n\n\n");
+            }
+
+     if(!strcmp(argv[1], "--about"))
+            {
+                printf("\n\n\nAbout : \n\n\tsvvitaly\n\tNsk, VKI, 2016\n\n\n");
+            }
+     return 0;
+ }
---
+++ overheard_cout.c
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+ int main(int argc, char **argv) {
+     if(argc <= 1) {
+         cout << "\n\n\nSyntax : \n\n\toverheard --vki\n\toverheard --about\n\n\n" << endl;
+         return 0;
+     }
+     if(!strcmp(argv[1], "--vki"))
+            {
+                
+cout << "\n\n                .:+oooo+/-" << endl;
+cout << "              `+o++++ooooo/" << endl;
+cout << "    .:////:.  +oo++++o++++/" << endl;
+cout << "  -+ooo++//+/:o+ooo++o+++oo+/+/....`" << endl;
+cout << " /o+++++:--:+o++ooooooo+++o+oooo++oo+:`" << endl;
+cout << ":oo++++:-----+oooooooo++++++ooo++++++oo-" << endl;
+cout << "ooooo++------://:----/+o+o+/--:/+o++++++" << endl;
+cout << "s+++oo+-------+/+:-::--:+:::     -o+++o:" << endl;
+cout << "+o+++o+-------:+y-`-../------     o++o/`" << endl;
+cout << "`+ooo++/::-----oM:-+:``/---///    ooo/+-" << endl;
+cout << " `ooo+oo+oo/--:+mNNd:` /--/::o-   /+o+/" << endl;
+cout << " /o+++++++oo/---:sdo. `:--yso-.    ```" << endl;
+cout << " :o+++++++ooo:------...---om/`" << endl;
+cout << "  :oooooooooo/-------------:--" << endl;
+cout << " `+ooooooo++o/-----------/---.     `.-:////::-`" << endl;
+cout << " /o++ooooooo+-----------:+```   `:++oooooooooo++-`" << endl;
+cout << " ++++oooooo:-------...--..    `:ooooooooooooooo+o+-" << endl;
+cout << " .+o++++o+oo-------     ``````+ooo+ooooo+++++ooo++o-" << endl;
+cout << "  `:+oooo++o------:```--------++osyhdhyoo++++++o++o/" << endl;
+cout << "    /oooo++:----------------.`-:yNMMdso++++++++oo+o-" << endl;
+cout << "    /ooos+:-------------------.-:mMho++++++++++oooo////:-`" << endl;
+cout << "     .--::-----------------------sNs+++++++++ooooo+++++oo+-" << endl;
+cout << "        `:-----------------------+Myo+++++++ooooooooo+++++o:" << endl;
+cout << "         .---------------------.-yMNho+++oooooo++++ooo+++++o" << endl;
+cout << "         .::---------------:-----mMMMmyoooooooooo++++o++++oo" << endl;
+cout << "        -:::::/----:-----:------:hdMMmyoooooooooo++++o+++oo-" << endl;
+cout << "        :::::os-----/::::::`-------hdo++++oooooo+++ooo+oo+." << endl;
+cout << "        -::::hs-----:::::::- -------o++++++ooooooooooo+:." << endl;
+cout << "        `::::::------:::::::  :-----:++++++ooooooo+++o+-" << endl;
+cout << "         -:::::------:::::::. -------/o+++++oo++o++++++o:" << endl;
+cout << "         `:::::------:::::::. .------:+o+++++o+ooooo+++o+" << endl;
+cout << "          `::::------:::::::. `-------.-+ooooo+ooooooooo." << endl;
+cout << "            .::------:::::::` .--------  .-/+oooooooo/." << endl;
+cout << "             .-------:::::::  ---------      :oooo++o-" << endl;
+cout << "             :-------:`````  .---------      oo++o+/-.::" << endl;
+cout << "            ---------:       `.......`        :+oooooo:" << endl;
+cout << "            ---------`                           `...\n\n" << endl;
+            }
+
+     if(!strcmp(argv[1], "--about"))
+            {
+                cout << "\n\n\nAbout : \n\n\tsvvitaly\n\tNsk, VKI, 2016\n\n\n" << endl;
+            }
+     return 0;
+ }
